bc83a9602168fb989e144f3a45018c8a41ad24cf
Flo <florent.vouin@gmail.com>
2023-05-05 11:54:53 +0200

    ✨ Define infrastructure-related workspace functions

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -1,3 +1,4 @@
 export * from './emulator.js';
 export * from './event-topic.js';
+export * from './infrastructure.js';
 export * from './project.js';
---
+++ src/definitions/infrastructure.ts
@@ -0,0 +1,139 @@
+import {
+  CliArgument,
+  CliCommand,
+  CliOption,
+  ParentCliCommandDefinition,
+} from '@causa/cli';
+import { WorkspaceFunction } from '@causa/workspace';
+import { AllowMissing } from '@causa/workspace/validation';
+import { IsBoolean, IsString } from 'class-validator';
+
+/**
+ * The `infrastructure` parent command, grouping all commands related to managing a project defining Infrastructure as
+ * Code.
+ */
+export const infrastructureCommandDefinition: ParentCliCommandDefinition = {
+  name: 'infrastructure',
+  description: 'Manages the lifecycle of infrastructure projects.',
+};
+
+/**
+ * The result of a {@link InfrastructurePrepare} operation.
+ */
+export type PrepareResult = {
+  /**
+   * The reference to the prepared deployment (e.g. the path to a Terraform plan).
+   */
+  output: string;
+
+  /**
+   * If `false`, no change was detected with the current infrastructure state, and deploying is not necessary.
+   */
+  isDeploymentNeeded: boolean;
+};
+
+/**
+ * Prepares a future deployment of the given infrastructure project.
+ * For example, for a Terraform infrastructure project, this would create the plan for a deployment.
+ * Returns a reference to the prepared deployment (e.g. the path to a Terraform plan).
+ */
+export abstract class InfrastructurePrepare extends WorkspaceFunction<
+  Promise<PrepareResult>
+> {
+  /**
+   * If `true` and changes have been prepared, they are printed to the standard output.
+   * This should probably not be used when calling this function programmatically.
+   */
+  @IsBoolean()
+  @AllowMissing()
+  readonly print?: boolean;
+
+  /**
+   * The location where the description of the future deployment (e.g. Terraform plan) should be written.
+   */
+  @IsString()
+  @AllowMissing()
+  readonly output?: string;
+}
+
+/**
+ * Deploys the infrastructure defined by a previous call to the {@link InfrastructurePrepare} function.
+ */
+export abstract class InfrastructureDeploy extends WorkspaceFunction<
+  Promise<void>
+> {
+  /**
+   * The deployment changes to deploy (e.g. the path to a Terraform plan).
+   */
+  @IsString()
+  readonly deployment!: string;
+}
+
+/**
+ * Wraps the call to the {@link InfrastructurePrepare} workspace function by first calling processors listed in the
+ * configuration `infrastructure.processors`.
+ * This is the function exposed from the CLI and it should not be implemented by any other than the core module.
+ * Returns a reference to the prepared deployment (e.g. the path to a Terraform plan).
+ */
+@CliCommand({
+  parent: infrastructureCommandDefinition,
+  name: 'prepare',
+  description: `Prepares a future deployment of an infrastructure project.
+After a deployment has been prepared, it can be deployed using the 'infrastructure deploy' command.`,
+  summary: 'Prepares a future deployment of an infrastructure project.',
+  outputFn: ({ output }) => console.log(output),
+})
+export abstract class InfrastructureProcessAndPrepare extends WorkspaceFunction<
+  Promise<PrepareResult>
+> {
+  /**
+   * If `true` and changes have been prepared, they are printed to the standard output.
+   * This should probably not be used when calling this function programmatically.
+   */
+  @IsBoolean()
+  @AllowMissing()
+  @CliOption({
+    flags: '-p, --print',
+    description:
+      'If set and changes have been prepared, they are printed to the standard output.',
+  })
+  readonly print?: boolean;
+
+  /**
+   * The location where the description of the future deployment (e.g. Terraform plan) should be written.
+   */
+  @IsString()
+  @AllowMissing()
+  @CliOption({
+    flags: '-o, --output <output>',
+    description:
+      'The location where the description of the future deployment should be written.',
+  })
+  readonly output?: string;
+}
+
+/**
+ * Wraps the call to the {@link InfrastructureDeploy} workspace function by first calling processors listed in the
+ * configuration `infrastructure.processors`.
+ * This is the function exposed from the CLI and it should not be implemented by any other than the core module.
+ */
+@CliCommand({
+  parent: infrastructureCommandDefinition,
+  name: 'deploy',
+  description: `Deploys the infrastructure defined by the output of the 'infrastructure prepare' command.`,
+  summary: `Deploys an infrastructure project.`,
+})
+export abstract class InfrastructureProcessAndDeploy extends WorkspaceFunction<
+  Promise<void>
+> {
+  /**
+   * The deployment changes to deploy (e.g. the path to a Terraform plan).
+   */
+  @IsString()
+  @CliArgument({
+    name: 'deployment',
+    position: 0,
+    description: `The output of the 'infrastructure prepare' command.`,
+  })
+  readonly deployment!: string;
+}

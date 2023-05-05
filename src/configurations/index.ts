8d66f6cfbca797413149d704a9d537f70b42d2d1
Flo <florent.vouin@gmail.com>
2023-05-05 15:57:14 +0200

    ✨ Define the Docker configuration type

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -1,2 +1,3 @@
 export { InfrastructureProjectConfiguration } from './infrastructure-project.js';
+export { DockerConfiguration } from './docker.js';
 export { ServiceContainerConfiguration } from './service-container-project.js';

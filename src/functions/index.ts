fbe0e64b050bb95182576d5a0fcea882f7793070
Flo <florent.vouin@gmail.com>
2023-05-15 15:58:51 +0200

    🐛 Register missing environment functions

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,6 +2,8 @@ import { ModuleRegistrationContext } from '@causa/workspace';
 import { EmulatorListForAll } from './emulator-list.js';
 import { EmulatorStartManyForAll } from './emulator-start-many.js';
 import { EmulatorStopManyForAll } from './emulator-stop-many.js';
+import { EnvironmentDeployForAll } from './environment-deploy.js';
+import { EnvironmentPrepareForAll } from './environment-prepare.js';
 import { ProjectPublishArtefactForAll } from './project-publish-artefact.js';
 import { ProjectPushArtefactForServiceContainer } from './project-push-artefact-service-container.js';
 import { SecretFetchForEnvironmentVariable } from './secret-fetch-environment-variable.js';
@@ -11,6 +13,8 @@ export function registerFunctions(context: ModuleRegistrationContext) {
     EmulatorListForAll,
     EmulatorStartManyForAll,
     EmulatorStopManyForAll,
+    EnvironmentDeployForAll,
+    EnvironmentPrepareForAll,
     ProjectPublishArtefactForAll,
     ProjectPushArtefactForServiceContainer,
     SecretFetchForEnvironmentVariable,

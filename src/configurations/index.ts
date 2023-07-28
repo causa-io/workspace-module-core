20bdfb208980ddc873f7e3fb88261d78f8da9999
Flo <florent.vouin@gmail.com>
2023-07-28 17:13:09 +0200

    ✨ Define the EventsConfiguration

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -1,5 +1,6 @@
 export { CausaConfiguration } from './causa.js';
 export { DockerConfiguration } from './docker.js';
+export { EventsConfiguration } from './events.js';
 export { InfrastructureConfiguration } from './infrastructure-project.js';
 export { ServerlessFunctionsConfiguration } from './serverless-functions-project.js';
 export { ServiceContainerConfiguration } from './service-container-project.js';

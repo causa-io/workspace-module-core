95b14a2ad81c8bc0c67f191cb1e62bf23dc0677a
Flo <florent.vouin@gmail.com>
2025-07-22 15:33:36 +0200

    ✨ Define the ModelGenerateCode and ModelRunCodeGenerator workspace functions

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -2,5 +2,6 @@ export * from './emulator.js';
 export * from './environment.js';
 export * from './event-topic.js';
 export * from './infrastructure.js';
+export * from './model.js';
 export * from './openapi.js';
 export * from './project.js';

64c05d0187578a737d6cd658e5ee3f0159f0e35e
Flo <florent.vouin@gmail.com>
2026-02-09 11:15:44 +0100

    ✨ Implement CausaListConfigurationSchemas for the core module

---
+++ src/functions/configuration/list-schemas.spec.ts
@@ -0,0 +1,30 @@
+import { BASE_CONFIGURATION_SCHEMA_PATH } from '@causa/workspace';
+import { createContext } from '@causa/workspace/testing';
+import 'jest-extended';
+import { basename } from 'path';
+import { CausaListConfigurationSchemas } from '../../definitions/index.js';
+import { CausaListConfigurationSchemasForCore } from './list-schemas.js';
+
+describe('CausaListConfigurationSchemasForCore', () => {
+  it('should return the configuration schemas for the core module', async () => {
+    const { context } = createContext({
+      configuration: { workspace: { name: 'test' } },
+      functions: [CausaListConfigurationSchemasForCore],
+    });
+
+    const actualSchemas = await context.call(CausaListConfigurationSchemas, {});
+
+    const actualBaseNames = actualSchemas.map((s) => basename(s));
+    expect(actualBaseNames).toIncludeSameMembers([
+      basename(BASE_CONFIGURATION_SCHEMA_PATH),
+      'causa.yaml',
+      'docker.yaml',
+      'events.yaml',
+      'infrastructure-project.yaml',
+      'model.yaml',
+      'openapi.yaml',
+      'serverless-functions-project.yaml',
+      'service-container-project.yaml',
+    ]);
+  });
+});
